3ef82ef59eb6b22997cc0f686e989e39705117b8
David <david.sanchez@inmediastudio.es>
2017-10-09 18:12:19 +0200

    laravel 55

---
+++ app/Models/Category.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Category
+ * @package App\Models
+ * @version October 9, 2017, 3:34 pm UTC
+ *
+ * @property string name
+ * @property string description
+ */
+class Category extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'categories';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required'
+    ];
+
+    
+}
